90542eb4ba9c0c56fd14f39bcd6cac5f2ad421f7
Abhishek Sahai <abhishek.sahai@outlook.in>
2022-08-01 18:55:53 +0530

    Map function on array

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const products = [
+    { name: "hardware", price: 1000 },
+    { name: "software", price: 2000 },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SahaiCart</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.name}>
+            {product.name} {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
