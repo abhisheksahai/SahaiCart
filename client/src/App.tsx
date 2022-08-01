67a89fbba89c0924fd679150f509a705d2616f20
Abhishek Sahai <abhishek.sahai@outlook.in>
2022-08-01 21:25:35 +0530

    Implemented useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 function App() {
-  const products = [
-    { name: "hardware", price: 1000 },
-    { name: "software", price: 2000 },
-  ];
+  const [products, setProducts] = useState([
+    { name: "Hardware", price: 1000 },
+    { name: "Software", price: 2000 },
+  ]);
+
+  function AddProduct() {
+    setProducts((previousState) => [
+      ...previousState,
+      {
+        name: "Product" + (previousState.length + 1),
+        price: (previousState.length + 1) * 1000,
+      },
+    ]);
+  }
+
   return (
     <div>
       <h1>SahaiCart</h1>
+      <button onClick={AddProduct}>Add product</button>
       <ul>
         {products.map((product) => (
           <li key={product.name}>
